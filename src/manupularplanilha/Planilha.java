e3f53b56124558d2e655ae94dcae3f400f58c9bb
Washington Leandro <sntslleandro@gmail.com>
2022-11-15 10:25:56 -0300

    Inserção do arquivo manipulando planilha

---
+++ src/manupularplanilha/Planilha.java
@@ -0,0 +1,29 @@
+package manupularplanilha;
+
+public class Planilha {
+
+	public static void main(String[] args) {
+		//OBSERVAÇÃO
+		/*Treinando manipulação de planilha
+		 * porem ainda há muito no que melhorar*/
+		int[][] planilha =
+		{
+			{1, 2, 3, 4, 5, 0},
+			{5, 6, 7, 8, 9, 0},
+			{1, 2, 3, 4, 5, 0},
+			{5, 6, 7, 8, 9, 0},
+			{1, 2, 3, 4, 5, 0}
+		
+		};
+		int somaTotal = 0;
+		for ( int i=0; i < planilha.length; i++) {
+			for (int j=0; j < planilha.length; j++) {
+				planilha[i][5] += planilha[i][j];
+			}
+			somaTotal += planilha[i][5];
+			System.out.println(somaTotal);
+		} 
+		System.out.println(somaTotal);
+	}
+
+}
